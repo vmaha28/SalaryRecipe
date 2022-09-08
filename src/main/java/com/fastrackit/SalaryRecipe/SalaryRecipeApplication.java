eea7566ab69c348636e7699edbf852d7d20f1022
Vlad <vlad.mahalean@gmail.com>
2022-09-08 15:28:49 +0300

    Structure commit.

---
+++ src/main/java/com/fastrackit/SalaryRecipe/SalaryRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.fastrackit.SalaryRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryRecipeApplication.class, args);
+	}
+
+}
